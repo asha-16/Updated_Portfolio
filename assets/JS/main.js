8aa3aa1abe2fbb6b196c4066fa248978bb336241
Asha_hp <ashahp.work@gmail.com>
2025-11-18 19:55:00 +0530

    Add services section

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -32,3 +32,42 @@ resumePortfolioTabBtns.forEach((resumePortfolioTabBtn, i) => {
 });
 
 
+
+/* ========================================
+   SERVICE MODAL OPEN/CLOSE FUNCTION
+======================================== */
+
+const serviceCardWithModals = document.querySelectorAll(".service-container.card-with-modal");
+
+serviceCardWithModals.forEach((serviceCardWithModal) => {
+    const serviceCard = serviceCardWithModal.querySelector(".service-card");
+    const serviceBackDrop = serviceCardWithModal.querySelector(".service-modal-backdrop");
+    const serviceModal = serviceCardWithModal.querySelector(".service-modal");
+    const modalCloseBtn = serviceCardWithModal.querySelector(".modal-close-btn");
+
+    serviceCard.addEventListener("click", () => {
+        serviceBackDrop.style.display = "flex";
+
+        setTimeout(() => {
+            serviceBackDrop.classList.add("active");
+        }, 100);
+
+        setTimeout(() => {
+            serviceModal.classList.add("active");
+        }, 100);
+        
+        modalCloseBtn.addEventListener("clicck", () => {
+
+            setTimeout(() => {
+                serviceBackDrop.style.display = "none";
+            }, 500);
+
+            setTimeout(() => {
+               serviceBackDrop.classList.remove("active");
+                serviceModal.classList.remove("active");
+            }, 100);
+            
+        });
+        
+    });
+});
